f90b3811e1be157a862b90015c5e94908106326c
Michael Jenkins <mjenkins9605@vidangel.com>
2019-05-12 22:19:39 -0400

    mvp

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const server = require('./server.js');
+const server = require("./server.js");
 
 server.listen(5678, () => {
-    console.log('port running on local host 5678')
+  console.log("port running on local host 5678");
 });
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const postRouter = require("./postRouter.js")
+const postRouter = require("./postRouter.js");
 const server = express();
 
 server.use(express.json());
-server.use('/api/posts', postRouter)
+server.use("/api/posts", postRouter);
 
 server.get("/", (req, res) => {
   res.send("It's alive!");
